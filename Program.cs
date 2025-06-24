c1af6cb90ad2b49c0b0ae9f53bc436aedbfc2bfc
Danilo Gonçalves Pinheiro <124277720+DaniloPin@users.noreply.github.com>
2025-06-24 10:47:37 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿Projeto projeto = new Projeto("Sistema de inventário");
+
+projeto.AdicionarTarefas("Criar tela de login.");
+projeto.AdicionarTarefas("Implementar banco de dados");
+
+projeto.ExibirTarefas();
---
+++ Projeto.cs
@@ -0,0 +1,32 @@
+﻿public class Projeto {
+
+    public string Nome { get; set; }
+    private List<string> tarefas;
+    private object get;
+
+    public Projeto(string nome) {
+        Nome = nome;
+        tarefas = new List<string>();
+    }
+
+    public void AdicionarTarefas(string tarefa) {
+        tarefas.Add(tarefa);
+    }
+
+    public void ExibirTarefas() {
+
+        Console.WriteLine($"Projeto: {Nome}\n");
+        Console.WriteLine($"Tarefa: ");
+
+        foreach (string tarefa in tarefas) {
+
+            Console.WriteLine($" - {tarefa}");
+        }
+        Console.WriteLine($"\nTotal: {QuantidadeTarefas} tarefas.");
+    }
+
+    public int QuantidadeTarefas {
+        
+        get { return tarefas.Count; }
+    }
+}
